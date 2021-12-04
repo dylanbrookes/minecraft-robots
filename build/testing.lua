6e3ad14d455cf04655b643c0fbddbb74f1a1364d
Dylan Brookes <dylan.brookes10@gmail.com>
2021-12-04 05:50:31 -0600

    tidy

---
+++ build/testing.lua
@@ -0,0 +1,6 @@
+_nodejs = true;
+local _ENV = require("castl.runtime");
+local module = _obj({exports = _obj({})});
+local exports = module.exports;
+console:log("this is a test");
+return module.exports;
