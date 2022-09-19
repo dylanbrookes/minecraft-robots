35284f8783b03e1efc3e141064fa9526280e9c22
dylanbrookes <dylan.brookes10@gmail.com>
2022-09-18 21:20:08 -0500

    update

--- build/chicken.lua
+++ build/chicken.lua
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream:build/chicken.lua
 --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 print("this is a chicken")
+=======
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+print("this is new")
+>>>>>>> Stashed changes:build/new.lua
