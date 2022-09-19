3904dde3e68aeb1763950c0ecf251cf419703fd5
Ryan Rossiter <nayr0788@gmail.com>
2022-09-19 17:33:04 -0400

    fix require_stub

--- build/models/Miner.lua
+++ build/models/Miner.lua
@@ -1,5 +1,4 @@
-local ____lualib = require("lualib_bundle")
-local __TS__Class = ____lualib.__TS__Class
+--[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
 local Direction = Direction or ({})
 Direction.left = "left"
--- build/require_stub.lua
+++ build/require_stub.lua
@@ -1,3 +1,9 @@
 local r = require("cc.require")
 _ENV.require, _ENV.package = r.make(_ENV, "/code")
 _ENV.args = { ... }
+
+-- apply lualib_bundle to global env
+local lb = require('/lualib_bundle')
+for k, v in pairs(lb) do
+  _ENV[k] = v;
+end
