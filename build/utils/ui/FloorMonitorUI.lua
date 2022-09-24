c4a93f5c267af65d0036e0bfc8e1ada688783376
Dylan Brookes <dylan.brookes10@gmail.com>
2022-09-24 02:12:10 +0000

    update manifest

--- build/utils/ui/FloorMonitorUI.lua
+++ build/utils/ui/FloorMonitorUI.lua
@@ -13,12 +13,13 @@ local FloorMonitorUI = ____exports.default
 FloorMonitorUI.name = "FloorMonitorUI"
 function FloorMonitorUI.prototype.____constructor(self, monitor, fps)
     if fps == nil then
-        fps = 0.1
+        fps = 0.5
     end
     self.monitor = monitor
     self.fps = fps
     self.id = 0
     self.registered = false
+    self.frame = 0
     local ____exports_default_0, ____ID_COUNTER_1 = ____exports.default, "ID_COUNTER"
     local ____exports_default_ID_COUNTER_2 = ____exports_default_0[____ID_COUNTER_1]
     ____exports_default_0[____ID_COUNTER_1] = ____exports_default_ID_COUNTER_2 + 1
@@ -58,28 +59,52 @@ function FloorMonitorUI.prototype.register(self)
     )
 end
 function FloorMonitorUI.prototype.render(self)
+    self.frame = self.frame + 1
     local oldterm = term.redirect(self.monitor)
-    local wu = bit32.arshift(self.width, 2)
-    local hu = bit32.arshift(self.height, 2)
-    do
-        local x = 0
-        while x < self.width / wu do
+    repeat
+        local ____switch9 = bit32.arshift(self.frame, 2 % 2)
+        local ____cond9 = ____switch9 == 0
+        if ____cond9 then
             do
-                local y = 0
-                while y < self.height / hu do
-                    paintutils.drawFilledBox(
-                        x * wu,
-                        y * hu,
-                        (x + 1) * wu,
-                        (y + 1) * hu,
-                        randomColor(nil)
-                    )
-                    y = y + 1
+                local wu = bit32.arshift(self.width, 2)
+                local hu = bit32.arshift(self.height, 2)
+                do
+                    local x = 0
+                    while x < self.width / wu do
+                        do
+                            local y = 0
+                            while y < self.height / hu do
+                                paintutils.drawFilledBox(
+                                    x * wu,
+                                    y * hu,
+                                    (x + 1) * wu,
+                                    (y + 1) * hu,
+                                    randomColor(nil)
+                                )
+                                y = y + 1
+                            end
+                        end
+                        x = x + 1
+                    end
                 end
             end
-            x = x + 1
+            break
         end
-    end
+        ____cond9 = ____cond9 or ____switch9 == 1
+        if ____cond9 then
+            do
+                self.monitor.setBackgroundColor(randomColor(nil))
+                self.monitor.clear()
+            end
+            break
+        end
+        ____cond9 = ____cond9 or ____switch9 == 2
+        if ____cond9 then
+            do
+            end
+            break
+        end
+    until true
     term.redirect(oldterm)
 end
 FloorMonitorUI.ID_COUNTER = 0
